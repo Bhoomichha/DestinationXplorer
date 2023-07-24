707ea9f39b4d4fcc36f4918b6831f3a47a02cf53
YuwenHung <yhung13@my.centennialcollege.ca>
2023-07-24 18:33:40 -0400

    trial angular commit

--- Destination_Xplorer/angular_project/README.md
+++ Destination_Xplorer/angular_project/README.md
@@ -1,4 +1,4 @@
-# AngularWebapp
+# AngularProject
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 16.1.5.
 
--- Destination_Xplorer/angular_project/src/app/app.component.spec.ts
+++ Destination_Xplorer/angular_project/src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'angular-webapp'`, () => {
+  it(`should have as title 'angular_project'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('angular-webapp');
+    expect(app.title).toEqual('angular_project');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('angular-webapp app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('angular_project app is running!');
   });
 });
