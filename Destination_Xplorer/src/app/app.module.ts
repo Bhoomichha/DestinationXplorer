2dea61a3c5d51629203b6e422779d2ee353e5339
Bhoomichha <bdangol@my.centennialcollege.ca>
2023-07-24 22:52:59 -0400

    commiting the components

--- Destination_Xplorer/src/app/app.module.ts
+++ Destination_Xplorer/src/app/app.module.ts
@@ -4,11 +4,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { ReviewPageComponent } from './review-page/review-page.component';
+import { ReviewComponent } from './review/review.component';
+import { SignupComponent } from './signup/signup.component';
+import { DestinationComponent } from './destination/destination.component';
+import { ContactsComponent } from './contacts/contacts.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    ReviewPageComponent,
+    ReviewComponent,
+    SignupComponent,
+    DestinationComponent,
+    ContactsComponent
   ],
   imports: [
     BrowserModule,
