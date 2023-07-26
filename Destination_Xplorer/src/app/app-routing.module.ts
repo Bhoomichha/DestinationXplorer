efaee7b0369e860a320bf77aaf560e99016de794
Ryan Yeung <yyeung22@my.centennialcollege.ca>
2023-07-25 21:06:14 -0400

    commiting the destination html

--- Destination_Xplorer/src/app/app-routing.module.ts
+++ Destination_Xplorer/src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { HomeComponent } from './home/home.component';
+import { ContactsComponent } from './contacts/contacts.component';
+import { DestinationComponent } from './destination/destination.component';
+import { ReviewComponent } from './review/review.component';
+import { SignupComponent } from './signup/signup.component';
+
+const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'contacts', component: ContactsComponent },
+  { path: 'destination', component: DestinationComponent },
+  { path: 'review', component: ReviewComponent },
+  { path: 'signup', component: SignupComponent },
+  { path: '', redirectTo: '/home', pathMatch: 'full' }, 
+  { path: '**', redirectTo: '/home' }, 
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
